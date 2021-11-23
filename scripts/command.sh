7b7efabbda37e362b9367abbe806b0d22742fd58
SteveYuOWO <littlepageprogram@163.com>
2021-11-24 04:49:56 +0800

    Update command

--- scripts/command.sh
+++ scripts/command.sh
@@ -1,6 +1,7 @@
 near dev-deploy --wasmFile res/fungible_token.wasm
 
-ID=dev-1637698879132-85915592042146
+# MASTER stevetoken.steveyu.testnet
+ID=stevetoken.steveyu.testnet
 near call $ID new_steve_token --accountId $ID
 near call $ID ft_metadata --accountId $ID
 near call $ID claim --accountId steveyu.testnet
@@ -11,3 +12,7 @@ near call $ID get_balance --accountId steveyu.testnet
 
 TXJSON='{"receiver_id": "steveyu.testnet", "amount": 10}'
 near call $ID transfer $TXJSON --accountId $ID
+
+near create-account stevetoken.steveyu.testnet --masterAccount steveyu.testnet
+near deploy stevetoken.steveyu.testnet res/fungible_token.wasm
+# https://explorer.testnet.near.org/accounts/stevetoken.steveyu.testnet
