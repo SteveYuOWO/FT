86e004991fa314214d28b766ad34a6dba48774db
SteveYuOWO <littlepageprogram@163.com>
2021-11-24 04:36:31 +0800

    Design STEVE token

--- ft/src/lib.rs
+++ ft/src/lib.rs
@@ -31,9 +31,12 @@ near_sdk::setup_alloc!();
 pub struct Contract {
     token: FungibleToken,
     metadata: LazyOption<FungibleTokenMetadata>,
+    master: AccountId,
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
+
+const STEVE_ICON: &str = "https://steveyuowo.com/static/profile-b272f5f50b822e954ad453368a71be91.png";
 
 #[near_bindgen]
 impl Contract {
@@ -69,10 +72,56 @@ impl Contract {
         let mut this = Self {
             token: FungibleToken::new(b"a".to_vec()),
             metadata: LazyOption::new(b"m".to_vec(), Some(&metadata)),
+            master: owner_id.to_string(),
         };
         this.token.internal_register_account(owner_id.as_ref());
         this.token.internal_deposit(owner_id.as_ref(), total_supply.into());
         this
+    }
+
+    #[init]
+    pub fn new_steve_token() -> Self {
+        let metadata = FungibleTokenMetadata {
+            spec: FT_METADATA_SPEC.to_string(),
+            name: "Steve Token".to_string(),
+            symbol: "STEVE".to_string(),
+            icon: Some(STEVE_ICON.to_string()),
+            reference: None,
+            reference_hash: None,
+            decimals: 24,
+        };
+        let mut this = Self {
+            token: FungibleToken::new(b"s".to_vec()),
+            metadata: LazyOption::new(b"t".to_vec(), Some(&metadata)),
+            master: env::predecessor_account_id().to_string(),
+        };
+        this.token.internal_register_account(&env::predecessor_account_id());
+        this.token.internal_deposit(&env::predecessor_account_id(), 1000_000_000);
+        this
+    }
+
+    pub fn claim(&mut self) {
+        self.token.internal_register_account(&env::predecessor_account_id());
+        self.token.internal_transfer(
+            &self.master,
+            &env::predecessor_account_id(),
+            1000,
+            None,
+        );
+        log!("claim 1000 STEVE token to {}", env::predecessor_account_id());
+    }
+
+    pub fn get_balance(&mut self) -> Balance {
+        self.token.internal_unwrap_balance_of(&env::predecessor_account_id())
+    }
+
+    pub fn transfer(&mut self, receiver_id: AccountId, amount: Balance) {
+        self.token.internal_transfer(
+          &env::predecessor_account_id(), 
+          &receiver_id, 
+          amount, 
+          None);
+        log!("transfer {} STEVE from {} to {}", amount, env::predecessor_account_id(), receiver_id);
     }
 
     fn on_account_closed(&mut self, account_id: AccountId, balance: Balance) {
--- scripts/command.sh
+++ scripts/command.sh
@@ -1,14 +1,6 @@
-STEVE Token
-===================
-
-## For near developmenet Challenge 6
-
-### Examples
-
-```
-./build.sh
 near dev-deploy --wasmFile res/fungible_token.wasm
 
+ID=dev-1637698879132-85915592042146
 near call $ID new_steve_token --accountId $ID
 near call $ID ft_metadata --accountId $ID
 near call $ID claim --accountId steveyu.testnet
@@ -19,5 +11,3 @@ near call $ID get_balance --accountId steveyu.testnet
 
 TXJSON='{"receiver_id": "steveyu.testnet", "amount": 10}'
 near call $ID transfer $TXJSON --accountId $ID
-```
-
