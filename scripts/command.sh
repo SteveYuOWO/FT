3d33dbdf7d46fc6ae4b29251e0c5add99041f890
SteveYuOWO <littlepageprogram@163.com>
2021-12-01 17:26:43 +0800

    Extend win & lose func

--- scripts/command.sh
+++ scripts/command.sh
@@ -6,9 +6,12 @@ near call $ID new_steve_token --accountId $ID
 near call $ID ft_metadata --accountId $ID
 near call $ID claim --accountId steveyu.testnet
 
-near call $ID get_balance --accountId $ID
+near call $ID get_balance --accountId steveyu.testnet
 # not registered
 near call $ID get_balance --accountId steveyu.testnet
+
+near call $ID win --accountId steveyu.testnet
+near call $ID lose --accountId steveyu.testnet
 
 TXJSON='{"receiver_id": "steveyu.testnet", "amount": 10}'
 near call $ID transfer $TXJSON --accountId $ID
